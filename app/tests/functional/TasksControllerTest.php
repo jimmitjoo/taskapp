ad8dcaf17a6ba8bafdd15ad6caacd89da4a88b93
jimmitjoo <jimmitjoo@gmail.com>
2014-12-08 18:43:18 +0100

    install phpunit, set up first test, its controller and the resource

---
+++ app/tests/functional/TasksControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jimmitjoo
+ * Date: 14-12-08
+ * Time: 18:29
+ */
+
+class TasksControllerTest extends TestCase {
+
+    public function test_this_test()
+    {
+        $this->assertTrue(true);
+    }
+
+} 
