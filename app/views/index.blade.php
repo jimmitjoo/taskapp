e5b7bac3f88867fca001b90314db15bedc4122d8
jimmitjoo <jimmitjoo@gmail.com>
2014-12-12 21:07:21 +0100

    import angular and set upp basic blade template

---
+++ app/views/index.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html ng-app>
+<head>
+    <title>Made With Love Test</title>
+</head>
+<body>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.27/angular.min.js"></script>
+    <script src="js/task.js"></script>
+</body>
+</html>
